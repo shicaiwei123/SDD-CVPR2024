400dcfde5d43e5c8dcc0d926f2b5dacbcaa1b6d3
shicaiwei <shicaiwei123@gmail.com>
2024-04-01 15:05:37 +0800

    Revert "update code for imagenet"
    This reverts commit 6d9e94eb4ba690046c54172984a8fd578c30ac65.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
 
-python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r34_r18/sdd_dkd.yaml --M [1,2] 72.02
-python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r50_mv2/sdd_dkd.yaml --M [1,2,4] 73.08
+python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r34_r18/sdd_dkd.yaml 72.02
+python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r50_mv2/sdd_dkd.yaml 73.08
 
 
-python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r50_mv2/sdd_nkd.yaml -- [1,2,4] 73.12
+python -m torch.distributed.launch --nproc_per_node=2 train.py --cfg ./configs/imagenet/r50_mv2/sdd_nkd.yaml 73.12
